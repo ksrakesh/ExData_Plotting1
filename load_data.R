fd59284d9c2f882bcea1e85d2f1b6cb36896310c
Rakesh Singh <Rakesh.Singh@med.fsu.edu>
2015-04-12 14:25:20 -0400

    Loading the dataset

--- load_data.R
+++ load_data.R
@@ -1,1 +1,18 @@
-
+## Loading the large dataset.
+filename <- "household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) # 2075259 9
+attach(data)
+## Subset the needed data.
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
--- plot2.R
+++ plot2.R
@@ -1,1 +1,13 @@
-
+## First check whether it has the file in the working dir.
+if (!"load_data.R" %in% list.files()) {
+  setwd("~/Desktop/Exploratory-Data-Analysis")
+} 
+source("load_data.R")
+png(filename = "plot2.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
